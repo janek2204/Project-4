03c08d6a01267b27ac6e8a5f91c2e87c21bb1753
piotr <piotr.jankowski2204@gmail.com>
2021-12-10 10:42:07 +0000

    ready to present

--- adverts/models.py
+++ adverts/models.py
@@ -8,9 +8,9 @@ CATEGORY_CHOICES = (("ELECTRONICS",'Electronics'),("CARS","Cars"),("HOME","Home"
 
 class Advert(models.Model):
   title = models.CharField(max_length=60)
-  description = models.TextField(max_length= 600)
+  description = models.TextField(max_length= 3000)
   quantity = models.IntegerField(default=1,)
-  images = models.CharField(max_length=500, blank=True)
+  images = models.CharField(max_length=700, blank=True)
   price = models.DecimalField(decimal_places=2,max_digits=10)
   category = models.CharField(choices=CATEGORY_CHOICES,default="OTHER",max_length=11)
   created_at = models.DateTimeField(auto_now_add=True)
