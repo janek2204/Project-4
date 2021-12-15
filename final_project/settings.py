e0802b92b1fcd867b18e4807c07a132c8f30cfd0
piotr <piotr.jankowski2204@gmail.com>
2021-12-15 20:12:15 +0000

    front and back end sorted before deploy

--- final_project/settings.py
+++ final_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0-v=agcb7)qbkq1*k(gwcue5bypc@dm5blclo!jn$@#$rqp0)2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
