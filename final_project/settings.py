a278e1ad7e2807bd0c04a92c225d4bbbeafa6f60
piotr <piotr.jankowski2204@gmail.com>
2021-12-15 20:58:36 +0000

    app deployed in heroku but dosent work

--- final_project/settings.py
+++ final_project/settings.py
@@ -128,6 +128,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
