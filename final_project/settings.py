4cfc8d9003cf992f73dbdae9b55c674561e0f727
piotr <piotr.jankowski2204@gmail.com>
2021-12-15 20:20:52 +0000

    front and back end sorted before deploy

--- final_project/settings.py
+++ final_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0-v=agcb7)qbkq1*k(gwcue5bypc@dm5blclo!jn$@#$rqp0)2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://easyshopp.herokuapp.com/']
 
 
 # Application definition
