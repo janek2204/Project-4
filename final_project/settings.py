a70fd09da4dfc518936328e961dd9e38a9c1ddc7
piotr <piotr.jankowski2204@gmail.com>
2021-12-17 19:58:39 +0000

    3 attempt to deploy project 4

--- final_project/settings.py
+++ final_project/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 )
 
-# STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'),)
+STATIC_ROOT = (os.path.join(BASE_DIR, 'static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
