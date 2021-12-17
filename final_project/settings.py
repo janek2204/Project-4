57bb14548f40b887d53c83d7187a4f0470b7f097
piotr <piotr.jankowski2204@gmail.com>
2021-12-17 19:56:26 +0000

    3 attempt to deploy project 4

--- final_project/settings.py
+++ final_project/settings.py
@@ -132,10 +132,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'post_images')
 ROOT_URLCONF = 'final_project.urls'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'frontend', 'build',),
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static'),
 )
 
-STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'),)
+# STATIC_ROOT = (os.path.join(BASE_DIR, 'staticfiles'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
