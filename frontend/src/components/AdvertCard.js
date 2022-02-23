ee9c82c402f2820826c1609a95c8b8e6842c1f74
piotr <piotr.jankowski2204@gmail.com>
2022-02-23 15:41:10 +0000

    removing console logs

--- frontend/src/components/AdvertCard.js
+++ frontend/src/components/AdvertCard.js
@@ -1,30 +1,26 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Card, Header, Segment, Image } from 'semantic-ui-react'
-import { useHistory } from 'react-router-dom'
+import React from "react";
+import { Card, Header, Segment, Image } from "semantic-ui-react";
+import { useHistory } from "react-router-dom";
 
 const AdvertCard = ({ title, price, images, id, category }) => {
+  const history = useHistory();
 
-  const [advertData, setAdvertData] = useState([])
-  const history = useHistory()
-  useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios.get(`/api/adverts/${id}/`)
-      setAdvertData(data)
-    }
-    getData()
-  }, [id])
-  console.log(advertData)
   return (
-    <Card  fluid onClick={() => history.push(`/adverts/${id}/`)}>
-      <Header as='h1' textAlign='left' > {title}</Header >
-      <Header as='h5' textAlign='left'>Category: {category}</Header>
+    <Card fluid onClick={() => history.push(`/adverts/${id}/`)}>
+      <Header as="h1" textAlign="left">
+        {title}
+      </Header>
+      <Header as="h5" textAlign="left">
+        Category: {category}
+      </Header>
       <Segment vertical>
-        <Header as='h3' textAlign='right'>Price: {price} £</Header>
+        <Header as="h3" textAlign="right">
+          Price: {price} £
+        </Header>
       </Segment>
-      <Image src={images}  />
+      <Image src={images} />
     </Card>
-  )
-}
+  );
+};
 
-export default AdvertCard
+export default AdvertCard;
