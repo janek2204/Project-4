071e0b476d1f21892b4ab761eb07e2d60f6e7cdc
piotr <piotr.jankowski2204@gmail.com>
2022-02-23 22:15:08 +0000

    removing console logs

--- frontend/src/components/Adverts.js
+++ frontend/src/components/Adverts.js
@@ -1,58 +1,66 @@
-import React, { useEffect, useState } from 'react'
-import { Container, Dropdown } from 'semantic-ui-react'
-import axios from 'axios'
-import AdvertCard from './AdvertCard'
+import React, { useEffect, useState } from "react";
+import { Container, Dropdown } from "semantic-ui-react";
+import axios from "axios";
+import AdvertCard from "./AdvertCard";
 
 const Adverts = () => {
-
-  const [adverts, setAdverts] = useState([])
-  const [filteredAdverts, setFilteredAdverts] = useState([])
+  const [adverts, setAdverts] = useState([]);
+  const [filteredAdverts, setFilteredAdverts] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
-      const { data } = await axios.get('api/adverts/')
-      setAdverts(data)
-    }
-    getData()
-  }, [])
+      const { data } = await axios.get("api/adverts/");
+      setAdverts(data);
+    };
+    getData();
+  }, []);
 
-  const handleClick = event => {
-    const filtering = adverts.filter(advert => {
-      return advert.category === event.target.id
-    })
-    setFilteredAdverts(filtering)
-  }
+  const handleClick = (event) => {
+    const filtering = adverts.filter((advert) => {
+      return advert.category === event.target.id;
+    });
+    setFilteredAdverts(filtering);
+  };
 
   const resetFilter = () => {
-    setFilteredAdverts([])
-  }
+    setFilteredAdverts([]);
+  };
 
   return (
     <>
-
       <Dropdown
-        text='Filter'
-        icon='filter'
+        text="Filter"
+        icon="filter"
         floating
         labeled
         button
-        className='icon'
+        className="icon"
       >
         <Dropdown.Menu>
-          <Dropdown.Header icon='tags' content='Filter by category' />
+          <Dropdown.Header icon="tags" content="Filter by category" />
           <Dropdown.Divider />
-          <Dropdown.Item id='ELECTRONICS' onClick={handleClick}>Electronics</Dropdown.Item>
-          <Dropdown.Item id='OTHER' onClick={handleClick}>Other</Dropdown.Item>
-          <Dropdown.Item id='CARS' onClick={handleClick}>Cars</Dropdown.Item>
-          <Dropdown.Item id='GARDEN' onClick={handleClick}>Garden</Dropdown.Item>
-          <Dropdown.Item id='HOME' onClick={handleClick}>Home</Dropdown.Item>
+          <Dropdown.Item id="ELECTRONICS" onClick={handleClick}>
+            Electronics
+          </Dropdown.Item>
+          <Dropdown.Item id="OTHER" onClick={handleClick}>
+            Other
+          </Dropdown.Item>
+          <Dropdown.Item id="CARS" onClick={handleClick}>
+            Cars
+          </Dropdown.Item>
+          <Dropdown.Item id="GARDEN" onClick={handleClick}>
+            Garden
+          </Dropdown.Item>
+          <Dropdown.Item id="HOME" onClick={handleClick}>
+            Home
+          </Dropdown.Item>
           <Dropdown.Divider />
-          <Dropdown.Header onClick={resetFilter} content='Show all adverts' />
+          <Dropdown.Header onClick={resetFilter} content="Show all adverts" />
         </Dropdown.Menu>
       </Dropdown>
-      {filteredAdverts.length > 0 ?
+      {filteredAdverts.length > 0 ? (
         <Container>
-          {filteredAdverts.map(advert => {
+          {filteredAdverts.map((advert) => {
             return (
               <AdvertCard
                 key={advert.id}
@@ -60,11 +68,14 @@ const Adverts = () => {
                 price={advert.price}
                 title={advert.title}
                 images={advert.images}
-                category={advert.category} />
-            )
-          })}</Container> :
+                category={advert.category}
+              />
+            );
+          })}
+        </Container>
+      ) : (
         <Container>
-          {adverts.map(advert => {
+          {adverts.map((advert) => {
             return (
               <AdvertCard
                 key={advert.id}
@@ -72,12 +83,14 @@ const Adverts = () => {
                 price={advert.price}
                 title={advert.title}
                 images={advert.images}
-                category={advert.category} />
-            )
-          })}</Container>
-      }
+                category={advert.category}
+              />
+            );
+          })}
+        </Container>
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Adverts
+export default Adverts;
